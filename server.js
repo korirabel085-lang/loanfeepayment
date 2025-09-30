2e9556e5473701d744491910d1843f431e83ad43
korirabel085-lang <korirabel085@gmail.com>
2025-09-30 17:56:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.post("/pay", async (req, res) => {
       external_reference: reference,
       customer_name: "Customer",
       callback_url: "https://swiftfee.onrender.com/callback",
-      channel_id: "000131"
+      channel_id: "000119"
     };
 
     const url = "https://swiftwallet.co.ke/pay-app-v2/payments.php";
